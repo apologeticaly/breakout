76d9771dc7392f804a26ae08727a06b618a1e52b
Constantino <muheec@gmail.com>
2020-01-23 13:27:21 -0800

    added challenge

--- scripts.js
+++ scripts.js
@@ -1,23 +1,23 @@
 /* eslint-disable no-alert */
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
-const ballRadius = 10;
+const ballRadius = 10*2;
 let x = canvas.width / 2;
-let y = canvas.height - 30;
-let dx = 2;
-let dy = -2;
-const paddleHeight = 10;
-const paddleWidth = 75;
+let y = canvas.height - 200;
+let dx = 5;
+let dy = -5;
+const paddleHeight = 10*2;
+const paddleWidth = 75*2;
 let paddleX = (canvas.width - paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
 const brickRowCount = 5;
 const brickColumnCount = 3;
-const brickWidth = 75;
-const brickHeight = 20;
-const brickPadding = 10;
-const brickOffsetTop = 30;
-const brickOffsetLeft = 30;
+const brickWidth = 75*2;
+const brickHeight = 20*2;
+const brickPadding = 10*2;
+const brickOffsetTop = 30*2;
+const brickOffsetLeft = 30*2;
 let score = 0;
 let lives = 3;
 
@@ -61,7 +61,7 @@ function collisionDetection() {
     for (let r = 0; r < brickRowCount; r += 1) {
       const b = bricks[c][r];
       if (b.status === 1) {
-        if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+        if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight + 20) {
           dy = -dy;
           b.status = 0;
           score += 1;
@@ -78,17 +78,27 @@ function collisionDetection() {
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-  ctx.fillStyle = '#0095DD';
+
+  if (score < 5) {
+    ctx.fillStyle = '#fff';
+  } else if (score > 5 || score > 13) {
+    ctx.fillStyle = '#121212';
+  } else {
+    ctx.fillStyle = '#fff';
+  }
+
   ctx.fill();
   ctx.closePath();
 }
+
 function drawPaddle() {
   ctx.beginPath();
   ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-  ctx.fillStyle = '#0095DD';
+  ctx.fillStyle = '#3FFF26';
   ctx.fill();
   ctx.closePath();
 }
+
 function drawBricks() {
   for (let c = 0; c < brickColumnCount; c += 1) {
     for (let r = 0; r < brickRowCount; r += 1) {
@@ -99,18 +109,42 @@ function drawBricks() {
         bricks[c][r].y = brickY;
         ctx.beginPath();
         ctx.rect(brickX, brickY, brickWidth, brickHeight);
-        ctx.fillStyle = '#0095DD';
+
+        if (c === 0) {
+          ctx.fillStyle = '#1DFF00';
+        } else if (c === 1) {
+          ctx.fillStyle = '#6DFF5A';
+        } else {
+          ctx.fillStyle = '#ABFFA0';
+        }
+
+        // switch (r) {
+        //   case 0:
+        //     // color A
+        //     break
+        //   case 1:
+        //       // color B
+        //     break
+        //   case 2:
+        //     // color C
+        //     break
+        //   default:
+        //     // color D
+        // }
+
         ctx.fill();
         ctx.closePath();
       }
     }
   }
 }
+
 function drawScore() {
   ctx.font = '16px Arial';
   ctx.fillStyle = '#0095DD';
   ctx.fillText(`Score: ${score}`, 8, 20);
 }
+
 function drawLives() {
   ctx.font = '16px Arial';
   ctx.fillStyle = '#0095DD';
@@ -131,7 +165,7 @@ function draw() {
   }
   if (y + dy < ballRadius) {
     dy = -dy;
-  } else if (y + dy > canvas.height - ballRadius) {
+  } else if (y + dy > canvas.height - (ballRadius + 10)) {
     if (x > paddleX && x < paddleX + paddleWidth) {
       dy = -dy;
     } else {
@@ -141,9 +175,9 @@ function draw() {
         document.location.reload();
       } else {
         x = canvas.width / 2;
-        y = canvas.height - 30;
-        dx = 3;
-        dy = -3;
+        y = canvas.height - 80;
+        dx = 5;
+        dy = -5;
         paddleX = (canvas.width - paddleWidth) / 2;
       }
     }
